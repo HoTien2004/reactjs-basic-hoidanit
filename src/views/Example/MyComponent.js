8b9e8728b343e47851de88bf6b5dcaa11da9aed6
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-11-05 01:56:13 +0700

    Component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>This is a simple React component.</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
