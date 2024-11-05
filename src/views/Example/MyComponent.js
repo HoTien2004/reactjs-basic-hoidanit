6962ab87927c994be328ea9c08cb9937b42dd410
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-11-06 00:12:02 +0700

    React dev tools

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Eric",
+    channel: "demo",
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+    // console.log(event.target.value); // for debugging purposes
+  };
+
   render() {
+    // let name = "Eric";
+
     return (
-      <div>
-        <h1>Hello, World!</h1>
-        <p>This is a simple React component.</p>
-      </div>
+      <>
+        <div>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleOnChange(event)}
+          />
+
+          <h1 className="abc">Hello, World! {this.state.name}</h1>
+        </div>
+      </>
     );
   }
 }
